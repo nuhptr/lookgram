d9d5ff4a59cb1bd4df5952f1a01bf4995fc86bae
nuhptr <nugrahaadi733@gmail.com>
2024-02-20 22:41:54 +0700

    shared components

---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+   return (
+      <div className="w-full flex-center">
+         <img src="/icons/ic_loader.svg" alt="Loader Icons" width={24} height={24} />
+      </div>
+   )
+}
